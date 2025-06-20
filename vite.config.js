d2d7c23905d983d1e22ad16bcff41d7b204f1531
AasmaNoor123 <aasmanoor511@gmail.com>
2025-06-20 07:19:30 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  base : "/Github-dev-finder",
-=======
->>>>>>> 5eb29e6cc72b04cf672eef67c00fdf6c47154789
+  base: "/Github-dev-finder/",
   plugins: [react()],
 })
